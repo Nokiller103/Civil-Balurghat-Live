b648d06fdcb8ed9a52404d4194cad1197bcac160
Nokiller103 <indikidanime@gmail.com>
2024-09-12 17:47:44 +0000

    fixing errors

--- src/jobs.js
+++ src/jobs.js
@@ -53,6 +53,7 @@ const JOBS = [
         location: 'West Bengal, India',
         image: 'https://images.unsplash.com/photo-1556228453-efd6c1ff04f6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
+    
 
 ]
 
