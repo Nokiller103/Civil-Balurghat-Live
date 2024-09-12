ac0719a21bd57343450093b419d1944b6713933b
Nokiller103 <indikidanime@gmail.com>
2024-09-12 10:26:10 +0000

    add mustache template and job.mustache file

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
 const express = require ('express');
 const path = require ('path');
+const JOBS = require('./jobs'); 
+const mustacheExpress = require('mustache-express');
 
 const app = express ();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('views',path.join(__dirname,'pages'));
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
+
 app.use(express.static(path.join(__dirname, 'images')));
 
 app.get('/', (req,res) => {
     //res.send('hello world!');
-    res.sendFile(path.join(__dirname, 'pages/index.html'));
+    //res.sendFile(path.join(__dirname, 'pages/index.html'));
+    res.render('index', {jobs:JOBS});
 });
+
+app.get('/jobs/:id', (req,res) => {
+    const id = req.param.id;
+    const matchedJob = JOBS.find(job => job.id.toString() === id);
+    res.render('jobs', {job:matchedJob});
+    })
+    
 
 const port = process.env.PORT || 3000;
 
---
+++ src/job.js
@@ -0,0 +1,59 @@
+const JOBS = [
+    {
+        Id: 1,
+        Title: '2D Design',
+        Location: 'West Bengal, India',
+        
+    },
+    {
+        Id: 2,
+        Title: '3D Exterior Design',
+        Location: 'West Bengal, India',
+        
+    },
+    {
+        Id: 3,
+        Title: '3D Interior Design ',
+        Location: ' West Bengal, India',
+        
+    },
+    {
+        Id: 4,
+        Title: 'Architects for Residential',
+        Location: 'West Bengal, India',
+        
+    },
+    {
+        Id: 5,
+        Title: 'Building Construction',
+        Location: 'West Bengal, India',
+        
+    },
+    {
+        Id: 6,
+        Title: 'Construction Consultant',
+        Location: 'West Bengal, India',
+        
+    },
+    {
+        Id: 7,
+        Title: 'Interior Designer Consultant',
+        Location: 'West Bengal, India',
+        
+    },
+    {
+        Id: 8,
+        Title: 'Building Renovation',
+        Location: 'West Bengal, India',
+        
+    },
+    {
+        Id: 9,
+        Title: 'Residence Interior Designer',
+        Location: 'West Bengal, India',
+        
+    },
+
+]
+
+module.exports = JOBS; 
